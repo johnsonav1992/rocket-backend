1766a6d3d04db837e8a089157006ea6d739b062a
Alex Johnson <johnsonav1992@gmail.com>
2024-02-19 12:30:31 -0600

    need to fix error

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+use rocket::fairing::AdHoc;
+use rocket::tokio::sync::RwLock;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Todo {
+    id: u64,
+    title: String,
+    completed: bool,
+}
+
+#[derive(Default)]
+struct AppState {
+    todos: RwLock<Vec<Todo>>,
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    let app_state = AppState::default();
+    rocket::build()
+        .attach(AdHoc::on_ignite("Todo API", |rocket| {
+            Ok(rocket.manage(app_state))
+        }))
+        .mount("/", routes![index])
 }
